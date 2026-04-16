873a6a3b740cf5aa9942847dcd7d53ee1badba12
RubenPari <rubenpari.developer@gmail.com>
2026-04-16 23:33:15 +0200

    feat(front): localize login page content

--- clear-songs-front/src/app/features/auth/login/login.component.ts
+++ clear-songs-front/src/app/features/auth/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component, inject } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [TranslateModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
